aed86b75a5be9bb4d123ece268b591cf57ada1c2
Charline8380 <charline.abuwe@gmail.com>
2021-10-19 21:26:42 +0200

    ajout beanDto et beanDo

--- backend/gespost/src/main/java/gespost/persistance/beanDo/PostDo.java
+++ backend/gespost/src/main/java/gespost/persistance/beanDo/PostDo.java
@@ -2,10 +2,78 @@ package gespost.persistance.beanDo;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 @Table(name = "posts")
 public class PostDo {
-    
-    
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String title;
+
+    private String content;
+
+    private boolean published;
+
+    private String tags;
+
+    public PostDo() {
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isPublished() {
+        return this.published;
+    }
+
+    public boolean getPublished() {
+        return this.published;
+    }
+
+    public void setPublished(boolean published) {
+        this.published = published;
+    }
+
+    public String getTags() {
+        return this.tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + " id='" + getId() + "'" + ", title='" + getTitle() + "'" + ", content='" + getContent() + "'"
+                + ", published='" + isPublished() + "'" + ", tags='" + getTags() + "'" + "}";
+    }
+
 }
